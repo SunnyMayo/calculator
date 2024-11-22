f4a9c0e86f69b308cb8caf7ce9be8b04ed91740a
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-22 16:29:10 +1100

    add eventlistener to buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttons = [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "*", "clear", "0", "=", "/"];
 const numpad = document.querySelector("#numpad");
+const display = document.querySelector("#display");
 
 document.addEventListener("DOMContentLoaded", () => {
     buttons.forEach(buttonText => {
@@ -8,16 +9,33 @@ document.addEventListener("DOMContentLoaded", () => {
         button.textContent = buttonText;
         if (parseInt(buttonText) || buttonText == 0) {
             button.classList.add("number");
+            button.addEventListener("click", () => {
+                if (!operator) {
+                    firstNum += buttonText;
+                }
+                else {
+                    secondNum += buttonText;
+                };
+            });
         }
         else if ("+-*/".includes(buttonText)) {
             button.classList.add("operator");
+            button.addEventListener("click", () => {
+                if (firstNum && !secondNum) {
+                    operator = buttonText;
+                };
+            });
         }
         else if (buttonText == "=") {
             button.classList.add("equals");
         }
         else {
             button.classList.add("clear");
+            button.addEventListener("click", () => {
+                display.textContent = "0";
+            });
         };
+
         numpad.appendChild(button);
     });
 });
@@ -41,5 +59,4 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
-
 

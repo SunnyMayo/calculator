4e231c86a4c808bf67c6ae9653766f1272094cdf
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-22 15:41:16 +1100

    add buttons to numpad using script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const buttons = [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "x", "clear", 0, "=", "/"];
+const numpad = document.querySelector("#numpad");
+
+document.addEventListener("DOMContentLoaded", () => {
+    buttons.forEach(buttonText => {
+        const button = document.createElement("button");
+        button.classList.add("button");
+        button.textContent = buttonText;
+        numpad.appendChild(button);
+    });
+});
+
 let firstNum;
 let secondNum;
 let operator;

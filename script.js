62c5ae76a59448df29db416539df7864bdcac148
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-11-24 17:14:15 +1100

    restyle of html, re-work of js file
    Redesigned html to include all required calculator buttons.
    Removed js scripts to create buttons as part of DOM.
    Restyled css file to accomodate new button classes.

--- script.js
+++ script.js
@@ -1,48 +1,9 @@
-const buttons = [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "*", "clear", "0", "=", "/"];
-const numpad = document.querySelector("#numpad");
 const display = document.querySelector("#display");
 
-document.addEventListener("DOMContentLoaded", () => {
-    buttons.forEach(buttonText => {
-        const button = document.createElement("button");
-        button.classList.add("button");
-        button.textContent = buttonText;
-        if (parseInt(buttonText) || buttonText == 0) {
-            button.classList.add("number");
-            button.addEventListener("click", () => {
-                if (!operator) {
-                    firstNum += buttonText;
-                }
-                else {
-                    secondNum += buttonText;
-                };
-            });
-        }
-        else if ("+-*/".includes(buttonText)) {
-            button.classList.add("operator");
-            button.addEventListener("click", () => {
-                if (firstNum && !secondNum) {
-                    operator = buttonText;
-                };
-            });
-        }
-        else if (buttonText == "=") {
-            button.classList.add("equals");
-        }
-        else {
-            button.classList.add("clear");
-            button.addEventListener("click", () => {
-                display.textContent = "0";
-            });
-        };
-
-        numpad.appendChild(button);
-    });
-});
-
-let firstNum;
+let firstNum = 0;
 let secondNum;
 let operator;
+
 
 function add(a, b) {
     return a + b;
@@ -60,3 +21,6 @@ function divide(a, b) {
     return a / b;
 };
 
+function percent(a) {
+    return a / 100;
+};
